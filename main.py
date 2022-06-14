2f6061403d1ccaa18969e7dd550f1dcdf856fb94
mateus <mateus.pereira4@gmail.com>
2022-06-14 16:46:01 -0300

    Alterações gerais

--- main.py
+++ main.py
@@ -14,44 +14,88 @@ c1.transfere_para(50,c2)
 c1.extrato()
 c2.extrato()'''
 
+clientes = []
+contas = []
+def cadastrar_cliente():
+    global clientes, contas, nome_ent, cpf_ent, dataNasc_ent, num_ent
+    clientes.append(Cliente(nome=nome_ent.get(), cpf=cpf_ent.get(), dataNasc=dataNasc_ent.get()))
 
-def cadastrar():
+    for i in range(len(clientes)):
+        if clientes[i].cpf == cpf_ent.get():
+            contas.append(Conta(clientes[i], num=num_ent.get()))
 
-    cliente = [Cliente(nome=nome_ent.get(), cpf=cpf_ent.get(), dataNasc=dataNasc_ent.get())]
 
+def entrar():
+    global contas, num_conta_ent
+    for i in range(len(contas)):
+        if contas[i].num == num_conta_ent.get():
+            print(clientes[i].nome, clientes[i].cpf, contas[i].num, contas[i].saldo)
 
-def consultar():
-
-    print(cliente)
+            tela_cliente.pack_forget()
+            tela_cliente_dados.pack()
+            cliente_nome['text'] += clientes[i].nome
+            cliente_cpf['text'] += clientes[i].cpf
+            cliente_dataNasc['text'] += clientes[i].dataNasc
+            cliente_num['text'] += contas[i].num
 
 
 root = Tk()
-cadastro_cliente = LabelFrame(root, text='Cadastro Cliente')
-cadastro_conta = LabelFrame(root, text='Cadastro Conta')
 
-nome = Label(cadastro_cliente, text='Nome:')
-cpf = Label(cadastro_cliente, text='CPF:')
-dataNasc = Label(cadastro_cliente, text='Data Nasc.:')
+tela_login = LabelFrame(root, text='Login')
+cadastro_cliente = LabelFrame(root, text='Cadastro')
+tela_cliente = LabelFrame(root, text='login')
+tela_cliente_dados = LabelFrame(root, text='dados')
+
+cliente = Button(tela_login, text='Cliente',  command=lambda: [tela_cliente.pack(), tela_login.pack_forget()])
+funcionario = Button(tela_login, text='Funcionario', command=lambda: [cadastro_cliente.pack(), tela_login.pack_forget()])
+
+nome = Label(cadastro_cliente, text='Nome: ')
+cpf = Label(cadastro_cliente, text='CPF: ')
+dataNasc = Label(cadastro_cliente, text='Data Nasc.: ')
+num = Label(cadastro_cliente, text='N° da conta: ')
 
 nome_ent = Entry(cadastro_cliente)
 cpf_ent = Entry(cadastro_cliente)
 dataNasc_ent = Entry(cadastro_cliente)
-cpf_consulta = Entry(cadastro_cliente)
+num_ent = Entry(cadastro_cliente)
 
-gravar = Button(cadastro_cliente, text='Cadastrar', command=lambda: cadastrar())
-listar = Button(cadastro_cliente, text='Consultar', command=lambda: consultar())
+gravar = Button(cadastro_cliente, text='Cadastrar', command=lambda: cadastrar_cliente())
+voltar = Button(cadastro_cliente, text='Voltar', command=lambda: [cadastro_cliente.pack_forget(), tela_login.pack()])
 
-cadastro_cliente.pack()
+num_conta = Label(tela_cliente, text='N° da Conta:')
+num_conta_ent = Entry(tela_cliente)
+conta_entrar = Button(tela_cliente, text='Entrar!', command=entrar)
+
+cliente_nome = Label(tela_cliente_dados, text='Nome:')
+cliente_cpf = Label(tela_cliente_dados, text='CPF:')
+cliente_dataNasc = Label(tela_cliente_dados, text='Data Nasc.:')
+cliente_num = Label(tela_cliente_dados, text='N° da conta:')
+
+tela_login.pack()
+
+cliente.pack(fill=X)
+funcionario.pack(fill=X)
+
 nome.grid(row=0, column=0)
 cpf.grid(row=1, column=0)
 dataNasc.grid(row=2, column=0)
+num.grid(row=3, column=0)
 
 nome_ent.grid(row=0, column=1)
 cpf_ent.grid(row=1, column=1)
 dataNasc_ent.grid(row=2, column=1)
-cpf_consulta.grid(row=4, column=1)
+num_ent.grid(row=3, column=1)
 
-gravar.grid(row=3, column=1)
-listar.grid(row=5, column=1)
+gravar.grid(row=4, column=1)
+voltar.grid(row=4, column=0)
+
+num_conta.grid(row=0, column=0)
+num_conta_ent.grid(row=0, column=1)
+conta_entrar.grid(row=1, column=1)
+
+cliente_nome.grid(row=0, column=0)
+cliente_cpf.grid(row=1, column=0)
+cliente_dataNasc.grid(row=2, column=0)
+cliente_num.grid(row=3, column=0)
 
 root.mainloop()
